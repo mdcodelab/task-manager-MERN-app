d87be1797911cb9ccdc6c4d1192249573746ce3c
miha11-hash <d_mihaela@msn.com>
2023-02-13 20:55:34 +0200

    create controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,34 @@
+
+
 const getAllTasks= (req, res) => {
-res.send("All items from the file")
+res.send("all tasks")
 }
 
+
+const createTask= (req, res) => {
+    const task=req.body
+    tasks.push(task)
+    res.send("create task")
+    }
+
+const getTask= (req, res) => {
+    
+res.send("get task")
+}
+
+const updateTask= (req, res) => {
+
+    res.send("update task")
+    }
+
+const deleteTask= (req, res) => {
+    res.send("delete task")
+    }
+
 module.exports = {
-    getAllTasks
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
 }
