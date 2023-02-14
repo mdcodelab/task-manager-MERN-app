0c18f50fd0e0dd9b750c19be554bf6940c10fb29
miha11-hash <d_mihaela@msn.com>
2023-02-14 21:16:26 +0200

    schema validation & testing

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -7,8 +7,12 @@ res.send("gel all items")
 
 
 const createTask= async (req, res) => {
-    const task = await Task.create(req.body)
-    res.status(201).json({task});
+   try {
+     const task = await Task.create(req.body)
+     res.status(201).json({task});
+   } catch (error) {
+    res.status(500).json({msg: error})
+   }
     }
 
 const getTask= (req, res) => {
--- models/task.js
+++ models/task.js
@@ -1,10 +1,19 @@
 const mongoose = require("mongoose")
 
 const taskSchema=new mongoose.Schema({
-    name: String, 
-    completed: Boolean
+    name: {
+        type: String,
+        required: [true, "You must provide a name"],
+        trim: true,
+        maxlength: [20, "Name cannot be more that 20 characters"]
+    }, 
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
-mongoose.set('strictQuery', true);
+//mongoose.set('strictQuery', true);
 
-module.exports=mongoose.model('Task', taskSchema)
+const Task=mongoose.model('Task', taskSchema)
+module.exports=Task
